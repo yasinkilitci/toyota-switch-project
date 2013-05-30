056061c209a3a918e881c8941061999eb96b3a0d
ilkeryasinyildiz@gmail.com <ilkeryasinyildiz@gmail.com@46ee9b67-0730-b334-220b-47bea92807d2>
2013-05-30 21:33:48 +0000

    son tutorial'ı kapsayan örnekler

--- trunk/src/org/spring/test/Triangle.java
+++ trunk/src/org/spring/test/Triangle.java
@@ -1,9 +1,51 @@
 package org.spring.test;
 
 public class Triangle {
+	
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 
-	public void draw(){
-		
-		System.out.println("I'm a triangle!");
-	}
+public void draw(){
+	
+	System.out.println("Point A = (" + pointA.getX() + "," + pointA.getY() + ")");
+	System.out.println("Point B = (" + pointB.getX() + "," + pointB.getY() + ")");
+	System.out.println("Point C = (" + pointC.getX() + "," + pointC.getY() + ")");
+}
+
+public Point getPointA() {
+	return pointA;
+}
+
+
+
+public void setPointA(Point pointA) {
+	this.pointA = pointA;
+}
+
+
+
+public Point getPointB() {
+	return pointB;
+}
+
+
+
+public void setPointB(Point pointB) {
+	this.pointB = pointB;
+}
+
+
+
+public Point getPointC() {
+	return pointC;
+}
+
+
+
+public void setPointC(Point pointC) {
+	this.pointC = pointC;
+}
+
+	
 }
